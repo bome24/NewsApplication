63d02953fccf8677682e7b0a0b9593d43aae3caf
bome24 <81296575+bome24@users.noreply.github.com>
2023-03-08 13:13:15 +0900

    FEAT: Set navigation bar color

--- GoodNewsApplication/AppDelegate.swift
+++ GoodNewsApplication/AppDelegate.swift
@@ -10,10 +10,22 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        if #available(iOS 13.0, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+            navBarAppearance.backgroundColor = UIColor(displayP3Red: 45 / 255, green: 52 / 255, blue: 54 / 255, alpha: 1.0)
+            UINavigationBar.appearance().standardAppearance = navBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        } else {
+            UINavigationBar.appearance().barTintColor = UIColor(displayP3Red: 45 / 255, green: 52 / 255, blue: 54 / 255, alpha: 1.0)
+            UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        }
+        
         return true
     }
 
---
+++ GoodNewsApplication/NewsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsTableViewController.swift
+//  GoodNewsApplication
+//
+//  Created by BoMin on 2023/03/08.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
